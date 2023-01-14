22386342656ad3b2a6448a25219d998c6988e9e4
akshatchawla1997 <95641311+akshatchawla1997@users.noreply.github.com>
2023-01-14 18:41:04 +0530

    Header added to the application

--- entairnment/src/App.js
+++ entairnment/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import { Header } from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ entairnment/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Header.css'
+export const Header = () => {
+     return ( 
+          <>
+          <div>
+               <header className='header'>
+                    Entertaintments
+               </header>
+          </div>
+          <h1>sdfghj</h1>
+          </>
+      );
+}
