44a631f7700629e5decb92022388411781f35bd4
akshatchawla1997 <95641311+akshatchawla1997@users.noreply.github.com>
2023-01-24 00:48:11 +0530

    badge added for rating

--- entairnment/src/Components/Header/SingleContent/SingleContent.js
+++ entairnment/src/Components/Header/SingleContent/SingleContent.js
@@ -1,3 +1,4 @@
+import { Badge } from '@mui/material'
 import React from 'react'
 import { img_300, unavailable } from '../../../config/Config'
 import './SingleContent.css'
@@ -6,6 +7,7 @@ const SingleContent = (props) => {
   return (
     <>
     <div className='media'>
+      <Badge badgeContent={props.vote_average} color={props.vote_average>6?'primary':'secondary'}/>
      <img className='poster' src={props.poster?`${img_300}/${props.poster}`: unavailable} alt={props.title}/>
      <b className='title'>{props.title}</b>
      <span className='subTitle'> {props.media_type === "tv"?"TV series":"Movie"}</span>
